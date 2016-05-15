7ecc1a616e5b67944d8f58a3c1ab3f81d686c3ee
NicoFuccella <niky.fuccella@gmail.com>
2016-05-15 13:43:24 +0200

    primo commit

---
+++ sintax/ciclo if/Progetto test/ProgettoTest/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProgettoTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ifButton_Click(object sender, EventArgs e)
+        {
+            string nome = "Pippo";
+            int anni = 28;
+
+            /* 
+             * a > b,
+             * a < b,
+             * a >= b,
+             * a <= b,
+             * a == b,
+             * a != b <-- DIVERSO
+             */
+
+            /*
+             * && <-- and
+             * || <-- or
+             */
+
+            if (nome == "Giovanni" && anni == 28)
+            {
+                Console.WriteLine("Ciao sei Giovanni");
+            }
+            else if (anni == 28)
+            {
+                Console.WriteLine("L'età è giusta ma non sei Giovanni");
+            }
+            else
+            {
+                Console.WriteLine("Non sei Giovanni");
+            }
+        }
+    }
+}
---
+++ sintax/ciclo if/README.md
@@ -0,0 +1,31 @@
+#CSharp
+
+##Ciclo if
+
+###Sintassi
+
+'''
+	if (*condizione*)
+	{
+		...
+	}
+	else if (*condizione*)
+	{
+		...
+	}
+	else
+	{
+		...
+	}
+'''
+
+###Ricorda
+*i simboli per i confonti sono
+	*'a > b'
+	*'a < b'
+	*'a >= b'
+	*'a <= b'
+	*'a == b'
+	*'a != b'
+*'and' --> '&&'
+*'or'  --> '||'
